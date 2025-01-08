ce0fc539a098687651c0546698e6f9390994219d
MuhammadRajaFadhilHabibi <muhammadrajafadhil@gmail.com>
2025-01-08 16:50:15 +0700

    fix add room

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class
+            UsersTableSeeder::class,
+            RoomSeeder::class,
         ]);
     }
 }
