f36f37d583d72d08ad71928ed65d0bea0f82868b
RahmanHanifs <128392694+RahmanHanifs@users.noreply.github.com>
2025-01-10 07:23:44 +0700

    perbaikan

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -38,22 +38,22 @@
                         <div class="card-body">
                             <div class="dash-widget-header">
                                 <div>
-                                    <h3 class="card_widget_header">236</h3>
-                                    <h6 class="text-muted">Total Booking</h6>
+                                    <h3 class="card_widget_header">{{ $count }}</h3>
+                                    <h6 class="text-muted">Total Booking diBulan {{ $currentMonthName }} </h6>
                                 </div>
                                 <div class="ml-auto mt-md-3 mt-lg-0"> 
-                                    <span class="opacity-7 text-muted">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewbox="0 0 24 24" fill="none" stroke="#FFBF00" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-user-plus">
-                                            <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                            <circle cx="8.5" cy="7" r="4"></circle>
-                                            <line x1="20" y1="8" x2="20" y2="14"></line>
-                                            <line x1="23" y1="11" x2="17" y2="11"></line>
-                                        </svg>
-                                    </span> 
-                                </div>
+                                        <span class="opacity-7 text-muted">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" 
+                                                stroke="#FFBF00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                                                class="feather feather-calendar">
+                                                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                                                <line x1="16" y1="2" x2="16" y2="6"></line>
+                                                <line x1="8" y1="2" x2="8" y2="6"></line>
+                                                <line x1="3" y1="10" x2="21" y2="10"></line>
+                                            </svg>
+                                        </span> 
+                                    </div>
+
                             </div>
                         </div>
                     </div>
